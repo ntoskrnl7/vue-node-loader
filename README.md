8d70840cf61e326cca3af9ae8911f5e415ceaf67
Jung-kwang Lee <jungkwang.lee@softcamp.co.kr>
2021-08-09 16:36:14 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vue-node-loader
+
+A [Node.js add-ons](https://nodejs.org/dist/latest/docs/api/addons.html) loader for Vue.
+
+Allows to connect native node modules with `.node` extension.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.default = loader;
 exports.raw = void 0;
@@ -13,21 +13,23 @@ var _schemaUtils = _interopRequireDefault(require("schema-utils"));
 var _options = _interopRequireDefault(require("./options.json"));
 
 function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : {
-    default: obj
-  };
+  return obj && obj.__esModule ? obj : { default: obj };
 }
 
 function loader(content) {
   const options = (0, _loaderUtils.getOptions)(this);
   (0, _schemaUtils.default)(_options.default, options, {
     name: "Node Loader for Vue",
-    baseDataPath: "options"
+    baseDataPath: "options",
   });
-  const name = (0, _loaderUtils.interpolateName)(this, typeof options.name !== "undefined" ? options.name : "[contenthash].[ext]", {
-    context: this.rootContext,
-    content
-  });
+  const name = (0, _loaderUtils.interpolateName)(
+    this,
+    typeof options.name !== "undefined" ? options.name : "[contenthash].[ext]",
+    {
+      context: this.rootContext,
+      content,
+    }
+  );
   this.emitFile(name, content);
   return `
 try {
@@ -36,9 +38,17 @@ try {
     const process = require('process');  
     const fs = require('fs');
     if (fs.existsSync(__dirname)) {
-      process.dlopen(module, __dirname + '/'  + ${JSON.stringify(name)}${typeof options.flags !== "undefined" ? `, ${JSON.stringify(options.flags)}` : ""});
+      process.dlopen(module, __dirname + '/'  + ${JSON.stringify(name)}${
+    typeof options.flags !== "undefined"
+      ? `, ${JSON.stringify(options.flags)}`
+      : ""
+  });
     } else {
-      process.dlopen(module, '${this.resourcePath}'${typeof options.flags !== "undefined" ? `, ${JSON.stringify(options.flags)}` : ""});
+      process.dlopen(module, '${this.resourcePath}'${
+    typeof options.flags !== "undefined"
+      ? `, ${JSON.stringify(options.flags)}`
+      : ""
+  });
     }
   } catch (error) {
     throw new Error('node-loader:\\n' + error);
